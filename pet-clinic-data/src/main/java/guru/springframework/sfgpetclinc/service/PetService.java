d8394f6616050689041e85b824f1f7c234c2eccc
ahmedaziz <ahmed83@me.com>
2018-10-28 14:58:29 +0100

    Implement interfaces for services. closes #5

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinc/service/PetService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinc.service;
+
+import guru.springframework.sfgpetclinc.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet owner);
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinc/service/VetService.java
@@ -0,0 +1,11 @@
+package guru.springframework.sfgpetclinc.service;
+
+import guru.springframework.sfgpetclinc.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet owner);
+    Set<Vet> findAll();
+}
